824837a532811dd1ebe47f6a661411832a58cc81
Leonardo Vitor de Witt <44558001+LvWitt@users.noreply.github.com>
2023-09-13 02:17:11 -0300

    fix router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
     <div class="app-area">
-        <MenuSidebar />
-        <div class="main-content"><RouterView /></div>
+        <RouterView />
     </div>
-    
 </template>
 
 <script>
-    import MenuSidebar from './components/MenuSidebar.vue';
+import MenuSidebar from './components/MenuSidebar.vue';
 
-    export default {
+export default {
     components: {
         MenuSidebar
     }
@@ -22,29 +20,17 @@ body {
     background-color: #1db954;
     background-image: linear-gradient(160deg, #1db954 0%, #80D0C7 100%);
     font-family: 'Open Sans', sans-serif;
-}    
+}
 
-.app-area{
+.function-header {
+    margin-left: 30px;
+}
+
+.app-area {
     display: flex;
     justify-content: center;
     width: 100%;
     height: 100%;
-    min-height: 100vh; 
-}
-
-.main-content{
-    background: rgba(36, 36, 36, 0.63);
-    border-radius: 12px;
-    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(0px);
-    -webkit-backdrop-filter: blur(0px);
-    width: 900px;
-    height: 800px;
-    margin: 10px 0px 0px 30px;
-    color: white;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    overflow: auto;
+    min-height: 100vh;
 }
 </style>
