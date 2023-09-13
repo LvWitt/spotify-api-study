f1c33f0b78afb47e1f6aa0908f517ad17baea8e6
Leonardo Vitor de Witt <44558001+LvWitt@users.noreply.github.com>
2023-09-13 02:33:03 -0300

    update styles

--- src/views/listeningHistory.vue
+++ src/views/listeningHistory.vue
@@ -1,88 +1,100 @@
 <template>
-    <div class="recentlyPlayedTracks">
-        <header class="function-header">
-            <h1>Recently Played Tracks</h1>
-        </header>
-        <div class="recentlyPlayed-content">
-            <div class="recentlyPlayed-data-container">
+    <MenuSidebar />
+    <div class="main-content">
+        <div class="recentlyPlayedTracks">
+            <header class="function-header">
+                <h1>Recently Played Tracks</h1>
+            </header>
+            <div class="recentlyPlayed-content">
+                <div class="recentlyPlayed-data-container">
 
                     <table class="recentlyPlayedSection">
-                            <tr>
-                                <th></th>
-                                <th>Track</th>
-                                <th>Artist</th>                   
-                                <th>Date</th>
-                            </tr>
-                            <tr v-for="recently in recentlyPlayed" :key="recently.track.name">
-                                <td class="recentlyPlayedImgSection">
-                                    <a :href="recently.track.external_urls.spotify" target="_blank">
+                        <tr>
+                            <th></th>
+                            <th>Track</th>
+                            <th>Artist</th>
+                            <th>Date</th>
+                        </tr>
+                        <tr v-for="recently in recentlyPlayed" :key="recently.track.name">
+                            <td class="recentlyPlayedImgSection">
+                                <a :href="recently.track.external_urls.spotify" target="_blank">
                                     <img class="recentlyPlayedImg" :src="recently.track.album.images[1].url"></a>
-                                </td>
-                                <td class="trackName">
-                                    {{recently.track.name}}
-                                </td>
-                                <td>
-                                    <b>{{recently.track.artists[0].name}}</b>
-                                </td>
-                                <td>
-                                    {{recently.played_at}}
-                                </td>
-                            </tr>
+                            </td>
+                            <td class="trackName">
+                                {{ recently.track.name }}
+                            </td>
+                            <td>
+                                <b>{{ recently.track.artists[0].name }}</b>
+                            </td>
+                            <td>
+                                {{ recently.played_at }}
+                            </td>
+                        </tr>
                     </table>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-   import axios from 'axios';
-   export default {
-    data(){
+import MenuSidebar from '../components/MenuSidebar.vue';
+import axios from 'axios';
+export default {
+    components: {
+        MenuSidebar,
+    },
+    data() {
         return {
             recentlyPlayed: []
         }
     },
     async created() {
         const accessToken = localStorage.getItem('access_token')
-            if (accessToken) {
-                const request = await axios.get('https://api.spotify.com/v1/me/player/recently-played?limit=50', { 
-                    headers: { 
-                        Authorization: `Bearer ${accessToken}` 
-                    } 
-                });
+        if (accessToken) {
+            const request = await axios.get('https://api.spotify.com/v1/me/player/recently-played?limit=50', {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
 
-                
-                this.recentlyPlayed = request.data.items;
-            }
-    } 
-   }
+
+            this.recentlyPlayed = request.data.items;
+        }
+    }
+}
 </script>
 
 <style>
-.recentlyPlayed-data-container ul, tr {
+.recentlyPlayed-data-container ul,
+tr {
     padding: 0;
     margin: 0;
 }
 
-.recentlyPlayed-data-container li, td {
+.recentlyPlayed-data-container li,
+td {
     margin-bottom: 5px;
     padding: 5px 10px;
-    background: #000000;
-    color: #666;
+    background: #00000086;
+    color: #fafafa;
     overflow: hidden;
     white-space: nowrap;
 }
 
-.recentlyPlayed-data-container li, td:first-child{
+.recentlyPlayed-data-container li,
+td:first-child {
     width: 60px;
     text-align: center;
 }
 
-.recentlyPlayed-data-container li, td:nth-child(2){
+.recentlyPlayed-data-container li,
+td:nth-child(2) {
     width: 200px;
 }
 
-.recentlyPlayed-data-container li, td:nth-child(3){
+.recentlyPlayed-data-container li,
+td:nth-child(3) {
     width: 120px;
 }
 
@@ -91,16 +103,37 @@
     font-size: 14px;
 }
 
-.recentlyPlayedImgSection{
+.recentlyPlayedImgSection {
     width: 60px;
 }
 
-.recentlyPlayedImg{
+.recentlyPlayedImg {
     width: 50px;
 }
 
-.trackName, .artistName{
+.trackName,
+.artistName {
     color: white;
 }
 
-</style>
+.recentlyPlayed-content {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+}
+
+.main-content {
+    background: rgba(36, 36, 36, 0.63);
+    border-radius: 12px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(0px);
+    -webkit-backdrop-filter: blur(0px);
+    width: 900px;
+    height: 800px;
+    margin: 10px 0px 0px 30px;
+    color: white;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    overflow: auto;
+}</style>
