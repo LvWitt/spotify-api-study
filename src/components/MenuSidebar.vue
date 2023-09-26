d6d015b3658a249bbb8188e79901b39b0a8d46e9
Leonardo Vitor de Witt <44558001+LvWitt@users.noreply.github.com>
2023-09-25 21:20:25 -0300

    Updated button styles

--- src/components/MenuSidebar.vue
+++ src/components/MenuSidebar.vue
@@ -5,7 +5,7 @@
                 <h1>Stats for Spotify</h1>
             </header>
             <div id="login-content">
-                    <p class="login-warning-menu"></p>
+                <p class="login-warning-menu"></p>
             </div>
         </div>
         <div class="options">
@@ -13,51 +13,137 @@
                 <button class="btn-topTracks" @click="$router.push('/TopTracks')">Top Tracks</button>
                 <button class="btn-topArtists" @click="$router.push('/TopArtists')">Top Artists</button>
                 <button class="btn-history" @click="$router.push('/ListeningHistory')">Listening History</button>
-                <button class="btn-recentlyPlayed" @click="$router.push('/Manager')">Manager</button>
-            </div>
-            <div class="fetch-options">
-                <label for="4weeks"> <input type="radio" value="short_term" id="4weeks"  name="topic"/> Last 4 weeks </label>
-                <label for="6months"> <input type="radio" value="medium_term" id="6months" name="topic"/> Last 6 months </label>
-                <label for="alltime"> <input type="radio" value="long_term" id="alltime" name="topic"/> All time </label>
+                <button class="btn-manager" @click="$router.push('/Manager')">Manager</button>
             </div>
         </div>
     </aside>
 </template>
 
 <style>
-    .main-header, .stats-options, .fetch-options{
-        margin: -1px -1px -1px -1px;
-        padding:20px 0px 20px 0px;
-    }
-    .stats-options {
-        display: flex;
-        gap: 10px;
-    }
+.btn-topTracks,
+.btn-topArtists,
+.btn-history,
+.btn-manager {
+    appearance: none;
+    backface-visibility: hidden;
+    background-color: #161616;
+    border-radius: 8px;
+    border-style: none;
+    box-shadow: rgba(39, 174, 96, .15) 0 4px 9px;
+    box-sizing: border-box;
+    color: #fff;
+    cursor: pointer;
+    display: inline-block;
+    font-family: Inter, -apple-system, system-ui, "Segoe UI", Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    font-weight: 600;
+    letter-spacing: normal;
+    line-height: 1.5;
+    outline: none;
+    overflow: hidden;
+    padding: 13px 20px;
+    position: relative;
+    text-align: center;
+    text-decoration: none;
+    transform: translate3d(0, 0, 0);
+    transition: all .3s;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    vertical-align: top;
+    white-space: nowrap;
+    width: 160px;
+}
 
-    stats-options, .fetch-options{
-        border: 1px solid black;
-    }
+.btn-manager:hover {
+  background-color: #3b3b3b;
+  opacity: 1;
+  transform: translateY(0);
+  transition-duration: .35s;
+}
 
-    .stats-options, .fetch-options{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }  
+.btn-history:hover {
+  background-color: #3b3b3b;
+  opacity: 1;
+  transform: translateY(0);
+  transition-duration: .35s;
+}
 
-    .menu-sidebar{
-        width: 230px;
-        height: 500px;
-        text-align: center;
-        border: 1px solid black;
-        margin: 20px 0px 0px 0px;
-        /* From https://css.glass */
-        background: rgba(36, 36, 36, 0.63);
-        border-radius: 10px;
-        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-        backdrop-filter: blur(0px);
-        -webkit-backdrop-filter: blur(0px);
-        color: white;
-    }  
-</style>
+.btn-topArtists:hover {
+  background-color: #3b3b3b;
+  opacity: 1;
+  transform: translateY(0);
+  transition-duration: .35s;
+}
+
+.btn-topTracks:hover {
+  background-color: #3b3b3b;
+  opacity: 1;
+  transform: translateY(0);
+  transition-duration: .35s;
+}
+
+
+.btn-manager:active {
+  transform: translateY(2px);
+  transition-duration: .35s;
+}
+
+.btn-history:active {
+  transform: translateY(2px);
+  transition-duration: .35s;
+}
+
+.btn-topArtists:active {
+  transform: translateY(2px);
+  transition-duration: .35s;
+}
+
+.button-68:active {
+  transform: translateY(2px);
+  transition-duration: .35s;
+}
+
+.btn-topTracks:active {
+  transform: translateY(2px);
+  transition-duration: .35s;
+}
+
+
+.main-header,
+.stats-options {
+    margin: -1px -1px -1px -1px;
+    padding: 20px 0px 20px 0px;
+}
+
+.stats-options {
+    display: flex;
+    gap: 10px;
+}
+
+stats-options {
+    border: 1px solid black;
+}
+
+.stats-options {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.menu-sidebar {
+    width: 230px;
+    height: 500px;
+    text-align: center;
+    border: 1px solid black;
+    margin: 20px 0px 0px 0px;
+    /* From https://css.glass */
+    background: rgba(36, 36, 36, 0.63);
+    border-radius: 10px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(0px);
+    -webkit-backdrop-filter: blur(0px);
+    color: white;
+}</style>
 
